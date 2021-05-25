a0c0bdd4b6e523b77f66848b5b66924c6dad7a39
reubenur-rahman <reubenur.rahman@hpe.com>
2021-05-25 21:49:49 +0530

    Add list functions for layouts and instance-types

--- pkg/client/library.go
+++ pkg/client/library.go
@@ -4,12 +4,13 @@ package client
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 	"net/url"
 	"strings"
 
+	consts "github.com/hpe-hcss/vmaas-cmp-go-sdk/pkg/common"
 	"github.com/hpe-hcss/vmaas-cmp-go-sdk/pkg/models"
 )
 
@@ -24,13 +25,13 @@ type LibraryApiService struct {
 }
 
 /*
-LibraryApiService
-Lists all library instance types
+LibrariesApiService
+Get All layouts
  * @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param serviceInstanceId
 
 */
-func (a *LibraryApiService) GetAllInstanceTypes(ctx context.Context, serviceInstanceId string) (*http.Response, error) {
+func (a *LibraryApiService) GetAllLayouts(ctx context.Context, serviceInstanceId string) (models.LayoutsResp, error) {
 	var (
 		localVarHttpMethod = strings.ToUpper("Get")
 		localVarPostBody   interface{}
@@ -39,8 +40,8 @@ func (a *LibraryApiService) GetAllInstanceTypes(ctx context.Context, serviceInst
 	)
 
 	// create path and map variables
-	localVarPath := a.Cfg.BasePath + "/v1/{service_instance_id}/library/instance-types"
-	localVarPath = strings.Replace(localVarPath, "{"+"service_instance_id"+"}", fmt.Sprintf("%v", serviceInstanceId), -1)
+	localVarPath := fmt.Sprintf("%s/%s/%s/%s", a.Cfg.Host, consts.VmaasCmpApiBasePath,
+		serviceInstanceId, consts.LibraryLayoutPath)
 
 	localVarHeaderParams := make(map[string]string)
 	localVarQueryParams := url.Values{}
@@ -78,18 +79,18 @@ func (a *LibraryApiService) GetAllInstanceTypes(ctx context.Context, serviceInst
 	}
 	r, err := a.Client.prepareRequest(ctx, localVarPath, localVarHttpMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
 	if err != nil {
-		return nil, err
+		return models.LayoutsResp{}, err
 	}
 
 	localVarHttpResponse, err := a.Client.callAPI(r)
 	if err != nil || localVarHttpResponse == nil {
-		return localVarHttpResponse, err
+		return models.LayoutsResp{}, err
 	}
 
 	localVarBody, err := ioutil.ReadAll(localVarHttpResponse.Body)
 	localVarHttpResponse.Body.Close()
 	if err != nil {
-		return localVarHttpResponse, err
+		return models.LayoutsResp{}, err
 	}
 
 	if localVarHttpResponse.StatusCode >= 300 {
@@ -102,33 +103,122 @@ func (a *LibraryApiService) GetAllInstanceTypes(ctx context.Context, serviceInst
 			err = a.Client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
 			if err != nil {
 				newErr.error = err.Error()
-				return localVarHttpResponse, newErr
+				return models.LayoutsResp{}, newErr
 			}
 			newErr.model = v
-			return localVarHttpResponse, newErr
-		}
-		if localVarHttpResponse.StatusCode == 404 {
-			var v models.ErrNotFound
-			err = a.Client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
-			if err != nil {
-				newErr.error = err.Error()
-				return localVarHttpResponse, newErr
-			}
-			newErr.model = v
-			return localVarHttpResponse, newErr
+			return models.LayoutsResp{}, newErr
 		}
 		if localVarHttpResponse.StatusCode == 500 {
 			var v models.ErrInternalError
 			err = a.Client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
 			if err != nil {
 				newErr.error = err.Error()
-				return localVarHttpResponse, newErr
+				return models.LayoutsResp{}, newErr
 			}
 			newErr.model = v
-			return localVarHttpResponse, newErr
+			return models.LayoutsResp{}, newErr
 		}
-		return localVarHttpResponse, newErr
+		return models.LayoutsResp{}, newErr
+	}
+    var layoutsResp models.LayoutsResp
+	if err = json.Unmarshal(localVarBody, &layoutsResp); err != nil {
+		return models.LayoutsResp{}, err
+	}
+	return layoutsResp, nil
+}
+
+func (a *LibraryApiService) GetAllInstanceTypes(ctx context.Context, serviceInstanceId string) (models.InstanceTypesResp, error) {
+	var (
+		localVarHttpMethod = strings.ToUpper("Get")
+		localVarPostBody   interface{}
+		localVarFileName   string
+		localVarFileBytes  []byte
+	)
+
+	// create path and map variables
+	localVarPath := fmt.Sprintf("%s/%s/%s/%s", a.Cfg.Host, consts.VmaasCmpApiBasePath,
+		serviceInstanceId, consts.LibraryInstanceTypesPath)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	// to determine the Content-Type header
+	localVarHttpContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHttpContentType := selectHeaderContentType(localVarHttpContentTypes)
+	if localVarHttpContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHttpContentType
 	}
 
-	return localVarHttpResponse, nil
+	// to determine the Accept header
+	localVarHttpHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHttpHeaderAccept := selectHeaderAccept(localVarHttpHeaderAccepts)
+	if localVarHttpHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHttpHeaderAccept
+	}
+	if ctx != nil {
+		// API Key Authentication
+		if auth, ok := ctx.Value(ContextAPIKey).(APIKey); ok {
+			var key string
+			if auth.Prefix != "" {
+				key = auth.Prefix + " " + auth.Key
+			} else {
+				key = auth.Key
+			}
+			localVarHeaderParams["Authorization"] = key
+
+		}
+	}
+	r, err := a.Client.prepareRequest(ctx, localVarPath, localVarHttpMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
+	if err != nil {
+		return models.InstanceTypesResp{}, err
+	}
+
+	localVarHttpResponse, err := a.Client.callAPI(r)
+	if err != nil || localVarHttpResponse == nil {
+		return models.InstanceTypesResp{}, err
+	}
+
+	localVarBody, err := ioutil.ReadAll(localVarHttpResponse.Body)
+	localVarHttpResponse.Body.Close()
+	if err != nil {
+		return models.InstanceTypesResp{}, err
+	}
+
+	if localVarHttpResponse.StatusCode >= 300 {
+		newErr := GenericSwaggerError{
+			body:  localVarBody,
+			error: localVarHttpResponse.Status,
+		}
+		if localVarHttpResponse.StatusCode == 401 {
+			var v models.ErrUnauthorized
+			err = a.Client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return models.InstanceTypesResp{}, newErr
+			}
+			newErr.model = v
+			return models.InstanceTypesResp{}, newErr
+		}
+		if localVarHttpResponse.StatusCode == 500 {
+			var v models.ErrInternalError
+			err = a.Client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return models.InstanceTypesResp{}, newErr
+			}
+			newErr.model = v
+			return models.InstanceTypesResp{}, newErr
+		}
+		return models.InstanceTypesResp{}, newErr
+	}
+	var instanceTypesResp models.InstanceTypesResp
+	if err = json.Unmarshal(localVarBody, &instanceTypesResp); err != nil {
+		return models.InstanceTypesResp{}, err
+	}
+	return instanceTypesResp, nil
 }
