0f8dda5b6f4320d099632fa6dfb50019a3e3751e
ravikumar29524 <ravi.kumar3@hpe.com>
2021-06-26 00:03:16 +0530

    Adding UTs for GetAllEnvironment

---
+++ pkg/client/environment_test.go
@@ -0,0 +1,137 @@
+// (C) Copyright 2021 Hewlett Packard Enterprise Development LP
+
+package client
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"reflect"
+	"testing"
+
+	gomock "github.com/golang/mock/gomock"
+	models "github.com/hpe-hcss/vmaas-cmp-go-sdk/pkg/models"
+)
+
+func TestEnvironmentApiService_GetAllEnvironment(t *testing.T) {
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	templateName := "test_template_get_all_environment"
+
+	tests := []struct {
+		name    string
+		param   map[string]string
+		given   func(m *MockAPIClientHandler)
+		want    models.GetAllEnvironment
+		wantErr bool
+	}{
+		{
+			name: "Normal Test case 1: Get all environments",
+			param: map[string]string{
+				"name": templateName,
+			},
+			given: func(m *MockAPIClientHandler) {
+				path := mockHost + "/v1/environments"
+				method := "GET"
+				headers := getDefaultHeaders()
+				req, _ := http.NewRequest(method, path, nil)
+				respBody := ioutil.NopCloser(bytes.NewReader([]byte(`
+					{
+						"environments": [{
+							"id": 1,
+							"name": "test_template_get_all_environment"
+						}]
+					}
+				`)))
+				m.EXPECT().prepareRequest(gomock.Any(), path, method, nil, headers, getUrlValues(map[string]string{
+					"name": templateName,
+				}), url.Values{}, "", nil).Return(req, nil)
+				m.EXPECT().callAPI(req).Return(&http.Response{
+					StatusCode: 200,
+					Body:       respBody,
+				}, nil)
+
+			},
+			want: models.GetAllEnvironment{
+				Environments: []models.GetEnvironment{
+					{
+						ID:   1,
+						Name: templateName,
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Failed Test case 2: Error in prepare request",
+			param: map[string]string{
+				"name": templateName,
+			},
+			given: func(m *MockAPIClientHandler) {
+				path := mockHost + "/v1/environments"
+				method := "GET"
+				headers := getDefaultHeaders()
+				m.EXPECT().prepareRequest(gomock.Any(), path, method, nil, headers, getUrlValues(map[string]string{
+					"name": templateName,
+				}), url.Values{}, "", nil).Return(nil, errors.New("prepare request error"))
+			},
+			want:    models.GetAllEnvironment{},
+			wantErr: true,
+		},
+		{
+			name: "Failed Test case 3: Error in calAPI",
+			param: map[string]string{
+				"name": templateName,
+			},
+			given: func(m *MockAPIClientHandler) {
+				path := mockHost + "/v1/environments"
+				method := "GET"
+				headers := getDefaultHeaders()
+				req, _ := http.NewRequest(method, path, nil)
+				respBody := ioutil.NopCloser(bytes.NewReader([]byte(`
+					{
+						"message": "Internal Server Error",
+						"recommendedActions": [
+							"Unknown error occurred. Please contact the administrator"
+						]
+					}
+				`)))
+				m.EXPECT().prepareRequest(gomock.Any(), path, method, nil, headers, getUrlValues(map[string]string{
+					"name": templateName,
+				}), url.Values{}, "", nil).Return(req, nil)
+				m.EXPECT().callAPI(req).Return(&http.Response{
+					StatusCode: 500,
+					Body:       respBody,
+				}, nil)
+
+			},
+			want:    models.GetAllEnvironment{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockAPIClient := NewMockAPIClientHandler(ctrl)
+			e := EnvironmentApiService{
+				Client: mockAPIClient,
+				Cfg: Configuration{
+					Host: mockHost,
+				},
+			}
+			tt.given(mockAPIClient)
+			got, err := e.GetAllEnvironment(ctx, tt.param)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("EnvironmentApiService.GetAllEnvironment() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("EnvironmentApiService.GetAllEnvironment() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
