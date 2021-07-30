fbdb9bed11b14c6dcd5c250e5076160fd51b2ab1
tshihad <shihad-t@hpe.com>
2021-07-30 04:32:06 -0700

    Introduced new approch to create API in sdk

---
+++ pkg/client/api.go
@@ -0,0 +1,88 @@
+package client
+
+import (
+	"context"
+	"io/ioutil"
+	"net/url"
+	"strings"
+)
+
+// all the required validation should be provided as validationFunc
+type validationFunc func() error
+
+// parse response as proper model
+type jsonPareserFunc func(body []byte) error
+
+type api struct {
+	method      string
+	path        string
+	client      APIClientHandler
+	jsonParser  jsonPareserFunc
+	validations []validationFunc
+}
+
+// do will call the API provided. this funtion will not return any response, but
+// response should be catched from jsonParser function itself
+func (a *api) do(ctx context.Context, request interface{}, queryParams map[string]string) error {
+	var (
+		localVarHTTPMethod = strings.ToUpper(a.method)
+		localVarFileName   string
+		localVarFileBytes  []byte
+	)
+	if a.path == "" || a.method == "" || a.client == nil || a.jsonParser == nil {
+		panic("api not properly configured")
+	}
+
+	for _, validations := range a.validations {
+		err := validations()
+		if err != nil {
+			return err
+		}
+	}
+	// create path and map variables
+	localVarPath := a.path
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := getURLValues(queryParams)
+	localVarFormParams := url.Values{}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+
+	r, err := a.client.prepareRequest(ctx, localVarPath, localVarHTTPMethod, request,
+		localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
+	if err != nil {
+		return err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(r)
+	if err != nil || localVarHTTPResponse == nil {
+		return err
+	}
+	if localVarHTTPResponse.StatusCode >= 300 {
+		return ParseError(localVarHTTPResponse)
+	}
+
+	localVarBody, err := ioutil.ReadAll(localVarHTTPResponse.Body)
+	defer localVarHTTPResponse.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	return a.jsonParser(localVarBody)
+}
--- pkg/client/clouds.go
+++ pkg/client/clouds.go
@@ -578,3 +578,34 @@ func (a *CloudsAPIService) GetAllCloudNetworks(ctx context.Context, cloudID,
 
 	return networkResp, nil
 }
+
+func (a *CloudsAPIService) GetAllCloudFolders(
+	ctx context.Context,
+	cloudID int,
+	queryParams map[string]string,
+) (models.GetAllCloudFolders, error) {
+	folders := models.GetAllCloudFolders{}
+
+	folderAPI := &api{
+		method: "GET",
+		path: fmt.Sprintf("%s/%s/%s/%d/%s",
+			a.Cfg.Host, consts.VmaasCmpAPIBasePath, consts.ZonePath, cloudID, consts.FolderPath),
+		client: a.Client,
+		// jsonParser also stores folder response, since folders is not a local variable
+		jsonParser: func(body []byte) error {
+			return json.Unmarshal(body, &folders)
+		},
+		// validate cloud id > 1
+		validations: []validationFunc{
+			func() error {
+				if cloudID < 1 {
+					return fmt.Errorf("%s", "cloud id should be greater than or equal to 1")
+				}
+				return nil
+			},
+		},
+	}
+
+	err := folderAPI.do(ctx, nil, queryParams)
+	return folders, err
+}
