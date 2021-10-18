95aee5ed691f63fa5c5c2c2a68bd6f8bdbc3dcb1
ravikumar29524 <ravi.kumar3@hpe.com>
2021-10-18 12:32:46 +0530

    Removing Unused files

--- pkg/client/keys_certs.go
+++
@@ -1,8 +0,0 @@
-// (C) Copyright 2021 Hewlett Packard Enterprise Development LP
-
-package client
-
-type KeysCertsAPIService struct {
-	Client APIClientHandler
-	Cfg    Configuration
-}
--- pkg/client/policies.go
+++
@@ -1,8 +0,0 @@
-// (C) Copyright 2021 Hewlett Packard Enterprise Development LP
-
-package client
-
-type PoliciesAPIService struct {
-	Client APIClientHandler
-	Cfg    Configuration
-}
