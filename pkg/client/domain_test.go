16cfb9e17be4daa6bf7be0cf55cd23b643526a62
ravikumar29524 <ravi.kumar3@hpe.com>
2021-10-20 15:33:27 +0530

    Adding UTs for domain.go

---
+++ pkg/client/domain_test.go
@@ -0,0 +1,248 @@
+//  (C) Copyright 2021 Hewlett Packard Enterprise Development LP
+
+package client
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"reflect"
+	"testing"
+
+	"github.com/HewlettPackard/hpegl-vmaas-cmp-go-sdk/pkg/models"
+	gomock "github.com/golang/mock/gomock"
+)
+
+func TestDomainAPIService_GetAllDomains(t *testing.T) {
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	templateName := "test_template_all_domains"
+	tests := []struct {
+		name    string
+		param   map[string]string
+		given   func(m *MockAPIClientHandler)
+		want    models.GetAllDomains
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Normal Test case 1: Get all domains",
+			param: map[string]string{
+				"name": templateName,
+			},
+			given: func(m *MockAPIClientHandler) {
+				path := mockHost + "/v1/networks/domains"
+				method := "GET"
+				headers := getDefaultHeaders()
+				req, _ := http.NewRequest(method, path, nil)
+				respBody := ioutil.NopCloser(bytes.NewReader([]byte(`
+					{
+						"networkDomains": [{
+							"id": 1,
+							"name": "test_template_all_domains"
+						}]
+					}
+				`)))
+				m.EXPECT().prepareRequest(gomock.Any(), path, method, nil, headers,
+					getURLValues(map[string]string{
+						"name": templateName,
+					}), url.Values{}, "", nil).Return(req, nil)
+
+				m.EXPECT().callAPI(req).Return(&http.Response{
+					StatusCode: 200,
+					Body:       respBody,
+				}, nil)
+			},
+			want: models.GetAllDomains{
+				NetworkDomains: []models.GetDomain{
+					{
+						ID:   1,
+						Name: templateName,
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Failed Test case 2: error in prepare request",
+			param: map[string]string{
+				"name": templateName,
+			},
+			given: func(m *MockAPIClientHandler) {
+				path := mockHost + "/v1/networks/domains"
+				method := "GET"
+				headers := getDefaultHeaders()
+				m.EXPECT().prepareRequest(gomock.Any(), path, method, nil, headers,
+					getURLValues(map[string]string{
+						"name": templateName,
+					}), url.Values{}, "", nil).Return(nil, errors.New("prepare request error"))
+			},
+			want:    models.GetAllDomains{},
+			wantErr: true,
+		},
+		{
+			name: "Failed Test case 3: error in callAPI",
+			param: map[string]string{
+				"name": templateName,
+			},
+			given: func(m *MockAPIClientHandler) {
+				path := mockHost + "/v1/networks/domains"
+				method := "GET"
+				headers := getDefaultHeaders()
+				req, _ := http.NewRequest(method, path, nil)
+				respBody := ioutil.NopCloser(bytes.NewReader([]byte(`
+					{
+						"message": "Internal Server Error",
+						"recommendedActions": [
+							"Unknown error occurred. Please contact the administrator"
+						]
+					}
+				`)))
+				m.EXPECT().prepareRequest(gomock.Any(), path, method, nil, headers,
+					getURLValues(map[string]string{
+						"name": templateName,
+					}), url.Values{}, "", nil).Return(req, nil)
+
+				m.EXPECT().callAPI(req).Return(&http.Response{
+					StatusCode: 500,
+					Body:       respBody,
+				}, nil)
+			},
+			want:    models.GetAllDomains{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockAPIClient := NewMockAPIClientHandler(ctrl)
+			d := DomainAPIService{
+				Client: mockAPIClient,
+				Cfg: Configuration{
+					Host: mockHost,
+				},
+			}
+			tt.given(mockAPIClient)
+			got, err := d.GetAllDomains(ctx, tt.param)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DomainAPIService.GetAllDomains() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DomainAPIService.GetAllDomains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDomainAPIService_GetSpecificDomain(t *testing.T) {
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	templateName := "test_template_get_a_specific_domain"
+	tests := []struct {
+		name     string
+		domainID int
+		given    func(m *MockAPIClientHandler)
+		want     models.GetSpecificDomain
+		wantErr  bool
+	}{
+		// TODO: Add test cases.
+		{
+			name:     "Normal Test case 1: Get a specific domain",
+			domainID: 1,
+			given: func(m *MockAPIClientHandler) {
+				path := mockHost + "/v1/networks/domains/1"
+				method := "GET"
+				headers := getDefaultHeaders()
+				req, _ := http.NewRequest(method, path, nil)
+				respBody := ioutil.NopCloser(bytes.NewReader([]byte(`
+					{
+						"networkDomain": {
+							"id": 1,
+							"name": "test_template_get_a_specific_domain"
+						}
+					}
+				`)))
+				m.EXPECT().prepareRequest(gomock.Any(), path, method, nil, headers,
+					getURLValues(nil), url.Values{}, "", nil).Return(req, nil)
+
+				m.EXPECT().callAPI(req).Return(&http.Response{
+					StatusCode: 200,
+					Body:       respBody,
+				}, nil)
+			},
+			want: models.GetSpecificDomain{
+				NetworkDomain: models.GetDomain{
+					ID:   1,
+					Name: templateName,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:     "Failed Test case 2: error in prepare request",
+			domainID: 1,
+			given: func(m *MockAPIClientHandler) {
+				path := mockHost + "/v1/networks/domains/1"
+				method := "GET"
+				headers := getDefaultHeaders()
+				m.EXPECT().prepareRequest(gomock.Any(), path, method, nil, headers,
+					getURLValues(nil), url.Values{}, "", nil).Return(nil, errors.New("prepare request error"))
+			},
+			want:    models.GetSpecificDomain{},
+			wantErr: true,
+		},
+		{
+			name:     "Failed Test case 3: error in callAPI",
+			domainID: 1,
+			given: func(m *MockAPIClientHandler) {
+				path := mockHost + "/v1/networks/domains/1"
+				method := "GET"
+				headers := getDefaultHeaders()
+				req, _ := http.NewRequest(method, path, nil)
+				respBody := ioutil.NopCloser(bytes.NewReader([]byte(`
+					{
+						"message": "Internal Server Error",
+						"recommendedActions": [
+							"Unknown error occurred. Please contact the administrator"
+						]
+					}
+				`)))
+				m.EXPECT().prepareRequest(gomock.Any(), path, method, nil, headers,
+					getURLValues(nil), url.Values{}, "", nil).Return(req, nil)
+
+				m.EXPECT().callAPI(req).Return(&http.Response{
+					StatusCode: 500,
+					Body:       respBody,
+				}, nil)
+			},
+			want:    models.GetSpecificDomain{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockAPIClient := NewMockAPIClientHandler(ctrl)
+			d := DomainAPIService{
+				Client: mockAPIClient,
+				Cfg: Configuration{
+					Host: mockHost,
+				},
+			}
+			tt.given(mockAPIClient)
+			got, err := d.GetSpecificDomain(ctx, tt.domainID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DomainAPIService.GetSpecificDomain() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DomainAPIService.GetSpecificDomain() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
