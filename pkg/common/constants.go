11454982448bdc11b477aa49e638945c07214020
tshihad <shihad-t@hpe.com>
2021-11-09 01:05:11 -0800

    change base path from v1 to v1beta1

--- pkg/common/constants.go
+++ pkg/common/constants.go
@@ -4,7 +4,7 @@ package common
 
 const (
 	// VmaasCmpAPIBasePath base cmp api path
-	VmaasCmpAPIBasePath = "v1"
+	VmaasCmpAPIBasePath = "v1beta1"
 	// InstancesPath
 	InstancesPath = "instances"
 	// InstanceTypesPath
