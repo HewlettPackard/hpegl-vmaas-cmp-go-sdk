10b11cf07df9bfe65b7ca372c487b5c31a83dd1e
Pushpalatha555 <pushpalatha.e-g@hpe.com>
2022-07-28 15:51:38 +0530

    updated the vs model

--- pkg/models/load_balancer.go
+++ pkg/models/load_balancer.go
@@ -717,26 +717,63 @@ type CreateLBVirtualServers struct {
 }
 
 type CreateLBVirtualServersReq struct {
-	ID                  int                  `json:"id" tf:"id,computed"`
-	LbID                int                  `json:"-" tf:"lb_id"`
-	Description         string               `json:"description" tf:"description"`
-	VipName             string               `json:"vipName" tf:"name"`
-	VipAddress          string               `json:"vipAddress" tf:"vip_address"`
-	VipProtocol         string               `json:"vipProtocol" tf:"type"`
-	VipPort             string               `json:"vipPort" tf:"vip_port"`
-	Pool                int                  `json:"pool" tf:"pool"`
-	VipHostName         string               `json:"vipHostName" tf:"vip_host_name"`
-	SSLServerCert       int                  `json:"sslServerCert" tf:"ssl_server_cert"`
-	SSLCert             int                  `json:"sslCert" tf:"ssl_client_cert"`
-	VirtualServerConfig *VirtualServerConfig `json:"config" tf:"config,sub"`
+	ID          int    `json:"id" tf:"id,computed"`
+	LbID        int    `json:"-" tf:"lb_id"`
+	Description string `json:"description" tf:"description"`
+	VipName     string `json:"vipName" tf:"name"`
+	VipAddress  string `json:"vipAddress" tf:"vip_address"`
+	Pool        int    `json:"pool" tf:"pool"`
+	VipPort     string `json:"vipPort" tf:"vip_port"`
+	VipHostName string `json:"vipHostName" tf:"vip_host_name"`
+
+	VipProtocol                  string                        `json:"vipProtocol" tf:"type"`
+	TcpApplicationProfileConfig  *TcpApplicationProfileConfig  `json:"-" tf:"tcp_application_profile,sub"`
+	UdpApplicationProfileConfig  *UdpApplicationProfileConfig  `json:"-" tf:"udp_application_profile,sub"`
+	HttpApplicationProfileConfig *HttpApplicationProfileConfig `json:"-" tf:"http_application_profile,sub"`
+
+	Persistence                      string                            `json:""  tf:"persistence"`
+	CookiePersistenceProfileConfig   *CookiePersistenceProfileConfig   `json:"-" tf:"cookie_persistence_profile,sub"`
+	SourceipPersistenceProfileConfig *SourceipPersistenceProfileConfig `json:"-" tf:"sourceip_persistence_profile,sub"`
+
+	SSLServerCert   int              `json:"sslServerCert" tf:"ssl_server_cert"`
+	SSLServerConfig *SSLServerConfig `json:"-" tf:"ssl_server_config,sub"`
+
+	SSLCert         int              `json:"sslCert" tf:"ssl_client_cert"`
+	SSLClientConfig *SSLClientConfig `json:"-" tf:"ssl_client_config,sub"`
+
+	VirtualServerConfig *VirtualServerConfig `json:"config"`
+}
+
+type TcpApplicationProfileConfig struct {
+	ApplicationProfile int `json:"-" tf:"application_profile"`
+}
+type UdpApplicationProfileConfig struct {
+	ApplicationProfile int `json:"-" tf:"application_profile"`
+}
+type HttpApplicationProfileConfig struct {
+	ApplicationProfile int `json:"-" tf:"application_profile"`
+}
+
+type CookiePersistenceProfileConfig struct {
+	PersistenceProfile int `json:"-" tf:"persistence_profile"`
+}
+type SourceipPersistenceProfileConfig struct {
+	PersistenceProfile int `json:"-" tf:"persistence_profile"`
+}
+
+type SSLClientConfig struct {
+	SSLClientProfile int `json:"-" tf:"ssl_client_profile"`
+}
+type SSLServerConfig struct {
+	SSLServerProfile int `json:"-" tf:"ssl_server_profile"`
 }
 
 type VirtualServerConfig struct {
-	Persistence        string `json:"persistence"  tf:"persistence"`
-	PersistenceProfile int    `json:"persistenceProfile" tf:"persistence_profile"`
-	ApplicationProfile int    `json:"applicationProfile" tf:"application_profile"`
-	SSLClientProfile   int    `json:"sslClientProfile" tf:"ssl_client_profile"`
-	SSLServerProfile   int    `json:"sslServerProfile" tf:"ssl_server_profile"`
+	Persistence        string `json:"persistence"`
+	PersistenceProfile int    `json:"persistenceProfile"`
+	ApplicationProfile int    `json:"applicationProfile"`
+	SSLClientProfile   int    `json:"sslClientProfile"`
+	SSLServerProfile   int    `json:"sslServerProfile"`
 }
 
 // CREATE LB Virtual Server Resp
