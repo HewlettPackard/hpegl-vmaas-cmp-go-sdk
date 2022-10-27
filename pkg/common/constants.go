295cc3a092e7cf9a08bac23d6a6ea0a71d6b58f1
manjunath-batakurki <manjunath-batakurki@hpe.com>
2022-10-27 05:22:53 -0700

    Update from v1bet1 to v1

--- pkg/common/constants.go
+++ pkg/common/constants.go
@@ -4,7 +4,7 @@ package common
 
 const (
 	// VmaasCmpAPIBasePath base cmp api path
-	VmaasCmpAPIBasePath = "v1beta1"
+	VmaasCmpAPIBasePath = "v1"
 	// InstancesPath
 	InstancesPath = "instances"
 	// InstanceTypesPath
