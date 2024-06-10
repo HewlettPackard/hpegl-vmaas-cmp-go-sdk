d9dfa49a0a4d9241ec7ef552eff7a36a08561c8b
Mahesh N <mahesh2@hpe.com>
2024-06-10 13:36:15 +0530

    update copywrite

--- pkg/models/instance_tf.go
+++ pkg/models/instance_tf.go
@@ -1,4 +1,4 @@
-// (C) Copyright 2021 Hewlett Packard Enterprise Development LP
+// (C) Copyright 2021-2024 Hewlett Packard Enterprise Development LP
 
 // This file contains struct models related to terraform, but not the API
 package models
